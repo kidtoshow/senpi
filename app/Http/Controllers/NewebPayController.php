5b9aa4e2c54a0c2b94ecd80a3f6aa4ff0828c5eb
toshow <kidtoshow@nextec.com.tw>
2024-12-04 03:52:01 +0800

    電子發票

--- app/Http/Controllers/NewebPayController.php
+++ app/Http/Controllers/NewebPayController.php
@@ -84,6 +84,48 @@ class NewebPayController extends Controller
                     'remark' => $remark
                 ]);
             }
+            //電子發票
+            if(!env('NEWEBPAY_TEST_MODE')) {
+                $post_data_array = array(
+                    //post_data 欄位資料
+                    'RespondType' => 'JSON',
+                    'Version' => '1.5',
+                    'TimeStamp' => time(), //請以 time() 格式
+                    'TransNum' => '',
+                    'MerchantOrderNo' => $order->first()->transactionId,
+                    'BuyerName' => $order->first()->user->name,
+                    'BuyerAddress' => $order->first()->user()->address,
+                    'BuyerEmail' => $order->first()->user->email,
+                    'Category' => 'B2C',
+                    'TaxType' => '1',
+                    'TaxRate' => '5',
+                    'Amt' => $order->first()->product->price*0.95,
+                    'TaxAmt' => $order->first()->product->price*0.05,
+                    'TotalAmt' => $order->first()->product->price,
+                    'CarrierType' => '',
+                    'CarrierNum' => rawurlencode(''),
+                    'LoveCode' => '',
+                    'PrintFlag' => 'Y',
+                    'ItemName' => $order->first()->product->name,
+                    'ItemCount' => '1',
+                    'ItemUnit' => '份',
+                    'ItemPrice' => $order->first()->product->price,
+                    'ItemAmt' => $order->first()->product->price,
+                    'Comment' => '',
+                    'CreateStatusTime' => '',
+                    'Status' => '1'
+                );
+                $post_data_str = http_build_query($post_data_array);
+                $post_data = trim(bin2hex(openssl_encrypt($this->addpadding($post_data_str), 'AES-256-CBC', env('INV_HASH_KEY'), OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING, env('INV_HASH_IV'))));
+                $url = 'https://inv.ezpay.com.tw/Api/invoice_issue';
+                $MerchantID = env('EZPAY_MERCHANT_ID');
+                $transaction_data_array = array(
+                    'MerchantID_' => $MerchantID,
+                    'PostData_' => $post_data
+                );
+                $transaction_data_str = http_build_query($transaction_data_array);
+                $result = $this->curl_work($url, $transaction_data_str);
+            }
         } else {
             // 支付失敗
             $order = PayOrder::where('transactionId', $result['Result']['MerchantTradeNo'])->get();
@@ -102,5 +144,42 @@ class NewebPayController extends Controller
     {
         logger($request->all());
         return redirect(route('pay-order-list'));
+    }
+
+    protected function addpadding($string, $blocksize = 32)
+    {
+        $len = strlen($string);
+        $pad = $blocksize - ($len % $blocksize);
+        $string .= str_repeat(chr($pad), $pad);
+        return $string;
+    }
+
+    protected function curl_work($url = '', $parameter = '')
+    {
+        $curl_options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_HEADER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_USERAGENT => 'ezPay',
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_POST => '1',
+            CURLOPT_POSTFIELDS => $parameter
+        );
+        $ch = curl_init();
+        curl_setopt_array($ch, $curl_options);
+        $result = curl_exec($ch);
+        $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $curl_error = curl_errno($ch);
+        curl_close($ch);
+        $return_info = array(
+            'url' => $url,
+            'sent_parameter' => $parameter,
+            'http_status' => $retcode,
+            'curl_error_no' => $curl_error,
+            'web_info' => $result
+        );
+        return $return_info;
     }
 }
